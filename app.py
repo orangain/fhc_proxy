8df827a755131938f51228bc8ef70c5b93a85bae
orangain <orangain@gmail.com>
2013-02-09 00:15:49 +0900

    天気を取得するAPIを追加した。
    /weather?city=CITY&date=DATE
    CITY: お天気Webサービスのcityに同じ。
          http://weather.livedoor.com/weather_hacks/webservice
    DATE: 今日|明日|明後日

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
+#coding: utf-8
+
 import os
-from flask import Flask
+import urllib
+import json
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/weather')
+def weather():
+    date = request.args['date']
+    city = request.args['city']
+
+    return _get_weather(date, city)
+
+def _get_weather(date, city):
+    url = 'http://weather.livedoor.com/forecast/webservice/json/v1?city=' + city
+
+    f = urllib.urlopen(url)
+    api_response = json.loads(f.read())
+
+    for forecast in api_response['forecasts']:
+        if forecast['dateLabel'] == date:
+            return u'%sのてんきは%sです' % (date, forecast['telop'])
+
+    return ''
+
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
